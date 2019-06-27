8b19d64d989f3adc0a70c2d9f464d258e5ffa1a6
Ricky <rickywing71@gmail.com>
2019-06-27 16:34:58 -0400

    HTML and CSS Refactor

---
+++ v2 Cleaner/script.js
@@ -0,0 +1,62 @@
+const calculateBtn = document.getElementById("calculate");
+calculateBtn.addEventListener("click", calculate);
+
+const leftovertext = document.getElementById("leftoveramount");
+
+function calculate() {
+    const total = document.getElementsByName("total")[0].value;
+    const funds = document.getElementsByName("fund");
+    if(total) {
+        let percentages = [];
+        let amounts = [];
+        for(let fund of funds){
+            let percentage = new Decimal(fund.value / 100);
+            percentages.push(percentage);
+            let amount = total * percentage;
+            // alert(amount);
+            amount = truncateToDecimals(amount);
+            // alert(amount);
+            amounts.push(amount);
+        }
+        if (checkIfHundredPercent(percentages)) {
+            let leftover = checkIfEqualsTotal(amounts, total);
+            leftovertext.innerText = leftover;
+            displayAmounts(amounts);
+        } else {
+            alert("Your percentage split doesn't equal 100");
+        }
+    } else {
+        alert("Please enter a total");
+    }
+}
+
+function checkIfHundredPercent(arr) {
+    let total = new Decimal(0);
+	arr.forEach(percent => {
+        total = Decimal.add(total, percent);
+    });
+    return total == 1;
+}
+
+function checkIfEqualsTotal(amounts, total) {
+    let amountTotal = new Decimal(0);
+    amounts.forEach(amount => {
+        amountTotal = Decimal.add(amountTotal, amount);
+    });
+    let leftover = Decimal.sub(total, amountTotal);
+    leftover = leftover.toFixed(2);
+    return leftover;
+}
+
+function displayAmounts(amounts) {
+    let funds = document.getElementsByClassName("amount");
+    for(let i = 0; i < funds.length; i++) {
+        funds[i].innerText = " $" + amounts[i];
+        funds[i].style.display = "inline";
+    }
+}
+
+function truncateToDecimals(num, dec = 2) {
+    const calcDec = Math.pow(10, dec);
+    return Math.trunc(num * calcDec) / calcDec;
+}
